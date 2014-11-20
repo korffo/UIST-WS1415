f6855cd0c4e6fe4c71e30fb268728521829364fc
9glaser <gerrit.glaser@gmail.com>
2014-11-20 13:01:54 +0100

    Verschiebung funktioniert jetzt auf Vertex Ebene.

--- ex3/TouchReceiver.cs
+++ ex3/TouchReceiver.cs
@@ -10,19 +10,20 @@ public class TouchReceiver : MonoBehaviour
 	Vector4[] vectorPositionUpdated;
 	int numberSupportedCursors = 2;
 	public Camera cam;
-
+	
 	//Transformationsmatrizen:
 	Matrix4x4 transformationMatrix = Matrix4x4.identity;
-
-	Matrix4x4 translationMatrix;
-	Matrix4x4 rotationMatrix;
-	Matrix4x4 scalingMatrix;
-
+	
+	Matrix4x4 translationMatrix = Matrix4x4.identity;
+	Matrix4x4 rotationMatrix = Matrix4x4.identity;
+	Matrix4x4 scalingMatrix = Matrix4x4.identity;
+	
 	public float rotWinkel = 12.0f;
-
+	
 	private Mesh mesh;
 	private Vector3[] vertices;
-
+	private Vector3[] verticesStart;
+	
 	
 	// Use this for initialization
 	void Start()
@@ -30,10 +31,12 @@ public class TouchReceiver : MonoBehaviour
 		pressedArray = new bool[numberSupportedCursors];
 		vectorPositionOld = new Vector4[numberSupportedCursors];
 		vectorPositionUpdated = new Vector4[numberSupportedCursors];
-
+	
 		mesh = GetComponent<MeshFilter>().mesh;
 		vertices = mesh.vertices;
-
+		//Erstellen einer Festen Kopie der Ausgangspositionen, damit es
+		//bei mehrmaliger Anwendung nicht zu weiteren Verschiebungen kommt
+		verticesStart = (Vector3[]) vertices.Clone();
 	}
 	
 	// Update is called once per frame
@@ -42,22 +45,22 @@ public class TouchReceiver : MonoBehaviour
 	{
 
 		Vector3 worldCoords = cam.ScreenToWorldPoint(vectorPositionOld[0]);
+		
+		//		Debug.Log("Transformations:Matrix: " + translationMatrix.ToString());
+		//		Debug.Log("Transformations:Position: " + gameObject.rigidbody.position);
+		//		Debug.Log("Koordinaten: " + translationMatrix.MultiplyVector(new Vector3()));
 
-//		Debug.Log("Transformations:Matrix: " + translationMatrix.ToString());
-//		Debug.Log("Transformations:Position: " + gameObject.rigidbody.position);
-//		Debug.Log("Koordinaten: " + translationMatrix.MultiplyVector(new Vector3()));
-
-		//Transformationsmatrix auf Vertices einzeln anwenden:
+		//Transformationen immer auf Basis der Ursprungsposition (in diesem Fall 0,0,0) durchführen
 		int j = 0;
 		while (j < vertices.Length) {
-			vertices[j] = transformationMatrix.MultiplyPoint(worldCoords);
+			vertices[j] = transformationMatrix.MultiplyPoint(verticesStart[j]);
 			j++;
 		}
-
+		
 		Debug.Log("Transformationsmatrix: " + transformationMatrix);
-
+		
 		//Debug.Log("Positionen: (" + pos1.x + "|" + pos1.y + ") (" + pos2.x + "|" + pos2.y + ")");
-
+		
 		mesh.vertices = vertices;
 		Debug.Log("vertices: ");
 		foreach (var item in vertices)
@@ -66,11 +69,8 @@ public class TouchReceiver : MonoBehaviour
 		}
 		mesh.RecalculateBounds();
 		mesh.RecalculateNormals();
-		
-		transform.position = vertices [0];
-
 	}
-
+	
 	/**
 	 * Event-Routine für jedes TUIO-Event.
 	 * Hier nur Transformations-Matrix berechnen.
@@ -89,34 +89,34 @@ public class TouchReceiver : MonoBehaviour
 				pressedArray [i] = true;
 				vectorPositionOld[i] = new Vector4(mycursor.getX()*Screen.width, Screen.height-mycursor.getY()*Screen.height, 1,0);
 				vectorPositionUpdated[i] = new Vector4(mycursor.getX()*Screen.width, Screen.height-mycursor.getY()*Screen.height, 1,0);
-			//TUIO-Event für Touch-Auflösung:
+				//TUIO-Event für Touch-Auflösung:
 			} else if (cursorEvent.state.Equals(BBCursorState.Remove)){
 				pressedArray [i] = false;
-		    //TUIO-Event für Position-Update:
+				//TUIO-Event für Position-Update:
 			} else {
 				vectorPositionUpdated[i] = new Vector4(mycursor.getX()*Screen.width, Screen.height-mycursor.getY()*Screen.height, 1,0);
 				//Berechnung wohl erst im Update loop
 				//Calculate new TransformationMatrix
 				//nächste ausgangsposition auf neue position setzen
 				//vectorPositionOld[i] = vectorPositionUpdated[i];
-
+				
 				//Translationsmatrix berechnen:
 				if (i == 0) {
 					Vector3 worldCoords = cam.ScreenToWorldPoint(vectorPositionUpdated[i]);
-	//				Debug.Log("TUIOCoords: " + vectorPositionUpdated[i]);
+					//				Debug.Log("TUIOCoords: " + vectorPositionUpdated[i]);
 					translationMatrix = new Matrix4x4();
-					translationMatrix.SetRow(0,new Vector4(1,0,0,-worldCoords.x));
-					translationMatrix.SetRow(1,new Vector4(0,1,0,-worldCoords.y));
+					translationMatrix.SetRow(0,new Vector4(1,0,0,worldCoords.x));
+					translationMatrix.SetRow(1,new Vector4(0,1,0,worldCoords.y));
 					translationMatrix.SetRow(2,new Vector4(0,0,1,0));
 					translationMatrix.SetRow(3,new Vector4(0,0,0,1));
 					//Debug.Log("Transformations:Matrix: " + translationMatrix.ToString());
 				}
-
+				
 				//Rotationsmatrix berechnen:
 				if (pressedArray[0] && pressedArray[1]) {
-
-				    //Winkel berchnen: ??
-				  
+					
+					//Winkel berchnen: ??
+					
 					//				Debug.Log("TUIOCoords: " + vectorPositionUpdated[i]);
 					rotationMatrix = new Matrix4x4();
 					rotationMatrix.SetRow(0,new Vector4(Mathf.Cos(rotWinkel),-Mathf.Sin(rotWinkel),0,0));
@@ -126,7 +126,7 @@ public class TouchReceiver : MonoBehaviour
 				}
 			}
 		} //end for
-
+		
 		//TransformationsMatrix aufbauen
 		transformationMatrix = translationMatrix; //rotationMatrix;//* translationMatrix;
 		
