a63cbfbb30d7d905dd28b0df40bc6b187a1f09d5
9glaser <gerrit.glaser@gmail.com>
2014-11-25 13:52:27 +0100

    Rotation, Skalierung und Translation klappen für sich genommmen ansheinend gut, jedoch in Kombination irgendwas+rotation nicht mehr.
    Die Rotation findet noch nicht im Ursprung statt.
    Fehler beim Skalieren wenn man den zweiten Finger wegnimmt, wieder hinsetzt und erneut skaliert (Sprung tritt auf, da die ausgangsvertices nur geschrieben werden wenn beide Finger entfernt wurden um unkontrolliertes verschieben zu vermeiden)

--- ex3/TouchReceiver.cs
+++ ex3/TouchReceiver.cs
@@ -18,9 +18,10 @@ public class TouchReceiver : MonoBehaviour
 	Matrix4x4 rotationMatrix = Matrix4x4.identity;
 	Matrix4x4 scalingMatrix = Matrix4x4.identity;
 	
-	public float rotWinkel = 12.0f;
-	
+	private float rotWinkel = 0;
+
 	private Mesh mesh;
+	private Vector3 vertexCenter;
 	private Vector3[] vertices;
 	private Vector3[] verticesStart;
 	
@@ -43,26 +44,42 @@ public class TouchReceiver : MonoBehaviour
 	//Hier nur Transformations-Matrix anwenden.
 	void Update()
 	{
+//		Debug.Log("vertices alt: ");
+//		foreach (var item in vertices)
+//		{
+//			Debug.Log("----: " + item);
+//		}
 
+		//rotWinkel++;
+		//rotWinkel = rotWinkel%360;
 		Vector3 worldCoords = cam.ScreenToWorldPoint(vectorPositionOld[0]);
 		
 		//		Debug.Log("Transformations:Matrix: " + translationMatrix.ToString());
 		//		Debug.Log("Transformations:Position: " + gameObject.rigidbody.position);
 		//		Debug.Log("Koordinaten: " + translationMatrix.MultiplyVector(new Vector3()));
 
+		//TODO:Move to Center for Rotation, Mittelpunkt immer Aktuell in Vector3 vertexCenter
+		// -> Verschiebung um vertexCenter, rotation, zurückSchieben?
+
+		
 		//Transformationen immer auf Basis der Ursprungsposition (in diesem Fall 0,0,0) durchführen
+		vertexCenter = Vector3.zero;
 		int j = 0;
 		while (j < vertices.Length) {
 			vertices[j] = transformationMatrix.MultiplyPoint(verticesStart[j]);
+			vertexCenter.x += vertices[j].x;
+			vertexCenter.y += vertices[j].y;
 			j++;
 		}
-		
-//		Debug.Log("Transformationsmatrix: " + transformationMatrix);
+		vertexCenter.x = vertexCenter.x / vertices.Length;
+		vertexCenter.y = vertexCenter.y / vertices.Length;
+		Debug.Log("Middle: (" + vertexCenter.x + "|" + vertexCenter.y + ")");
+		//	Debug.Log("Transformationsmatrix: " + transformationMatrix);
 		
 		//Debug.Log("Positionen: (" + pos1.x + "|" + pos1.y + ") (" + pos2.x + "|" + pos2.y + ")");
 		
 		mesh.vertices = vertices;
-//		Debug.Log("vertices: ");
+//		Debug.Log("vertices neu: ");
 //		foreach (var item in vertices)
 //		{
 //			Debug.Log("----: " + item);
@@ -70,18 +87,18 @@ public class TouchReceiver : MonoBehaviour
 		mesh.RecalculateBounds();
 		mesh.RecalculateNormals();
 
-
-//		//Keine Toucheventsvorahnden
-//		int activeEvents = 0;
-//		foreach(bool b in pressedArray){
-//			if (b){
-//				activeEvents++;
-//			}
-//		}
-//
-//		if(activeEvents == 0){
-//			verticesStart = (Vector3[]) vertices.Clone();
-//		}
+		//Matritzen Zurücksetzen, Neue Ausgangsposition Setzen
+		if (!pressedArray [0])
+		{
+			scalingMatrix = Matrix4x4.identity;
+			rotationMatrix = Matrix4x4.identity;
+			translationMatrix = Matrix4x4.identity;
+			transformationMatrix = Matrix4x4.identity;
+			//vectorPositionOld [0] = vectorPositionUpdated [0];
+			//vectorPositionOld [1] = vectorPositionUpdated [1];
+			
+			verticesStart = (Vector3[]) vertices.Clone();
+		}
 	}
 	
 	/**
@@ -108,6 +125,7 @@ public class TouchReceiver : MonoBehaviour
 				//TUIO-Event für Position-Update:
 			} else {
 				vectorPositionUpdated[i] = new Vector4(mycursor.getX()*Screen.width, Screen.height-mycursor.getY()*Screen.height, 1,0);
+				//vectorPositionUpdated[i] = new Vector4(mycursor.getX(), mycursor.getY(), 1,0);
 				//Berechnung wohl erst im Update loop
 				//Calculate new TransformationMatrix
 				//nächste ausgangsposition auf neue position setzen
@@ -115,9 +133,11 @@ public class TouchReceiver : MonoBehaviour
 				
 				//Translationsmatrix berechnen:
 				if (i == 0) {
-					Vector3 worldCoords = cam.ScreenToWorldPoint(vectorPositionUpdated[i]);
-					//				Debug.Log("TUIOCoords: " + vectorPositionUpdated[i]);
-					translationMatrix = new Matrix4x4();
+					//Verschiebung bestimmen
+					Vector3 worldCoordsOld = cam.ScreenToWorldPoint(vectorPositionOld[i]);
+					Vector3 worldCoordsNew = cam.ScreenToWorldPoint(vectorPositionUpdated[i]);
+					Vector3 worldCoords = worldCoordsNew - worldCoordsOld;
+				
 					translationMatrix.SetRow(0,new Vector4(1,0,0,worldCoords.x));
 					translationMatrix.SetRow(1,new Vector4(0,1,0,worldCoords.y));
 					translationMatrix.SetRow(2,new Vector4(0,0,1,0));
@@ -131,17 +151,27 @@ public class TouchReceiver : MonoBehaviour
 					//Winkel berchnen: ??
 					
 					//				Debug.Log("TUIOCoords: " + vectorPositionUpdated[i]);
-					rotWinkel = Vector3.Angle(vectorPositionUpdated[0],vectorPositionUpdated[1]);
+					//Annahme: Drehung um Fixpunkt Old[0]
+					Vector3 richtungsVektor1 = vectorPositionOld[0]-vectorPositionOld[1];
+					Vector3 richtungsVektor2 = vectorPositionUpdated[0]-vectorPositionUpdated[1];
+					//rotWinkel = Vector3.Angle(vectorPositionUpdated[0],vectorPositionUpdated[1]);
+					rotWinkel = Vector3.Angle(richtungsVektor1,richtungsVektor2);
+
+					//Winkel in Rad benötigt
+					float rotCalculated = rotWinkel * Mathf.PI / 180;
+					//Debug.Log("Winkel: " + rotWinkel+"- Offset: " +rotWinkelOffset + "- Berechnet: " + rotCalculated);
+
+
 					rotationMatrix = new Matrix4x4();
-					rotationMatrix.SetRow(0,new Vector4(Mathf.Cos(rotWinkel),-Mathf.Sin(rotWinkel),0,0));
-					rotationMatrix.SetRow(1,new Vector4(Mathf.Sin(rotWinkel), Mathf.Cos(rotWinkel),0,0));
+					rotationMatrix.SetRow(0,new Vector4(Mathf.Cos(rotCalculated),-Mathf.Sin(rotCalculated),0,0));
+					rotationMatrix.SetRow(1,new Vector4(Mathf.Sin(rotCalculated), Mathf.Cos(rotCalculated),0,0));
 					rotationMatrix.SetRow(2,new Vector4(0,0,1,0));
 					rotationMatrix.SetRow(3,new Vector4(0,0,0,1));
 
 					//Abstände berechnen
 					float d1 = Vector3.Distance(vectorPositionOld[0], vectorPositionOld[1]);
 					float d2 = Vector3.Distance(vectorPositionUpdated[0], vectorPositionUpdated[1]);
-					Debug.Log("Abstände: - alt "+d1+" - neu "+d2);
+					//Debug.Log("Abstände: - alt "+d1+" - neu "+d2);
 					scalingMatrix = new Matrix4x4();
 					scalingMatrix.SetRow(0,new Vector4(d2/d1,0,0,0));
 					scalingMatrix.SetRow(1,new Vector4(0,d2/d1,0,0));
@@ -149,11 +179,15 @@ public class TouchReceiver : MonoBehaviour
 					scalingMatrix.SetRow(3,new Vector4(0,0,0,1));
 				}
 			}
+			//Debug.Log("Positionen: Old ("+vectorPositionOld[i].x+"|"+vectorPositionOld[i].y+") - Updated ("+vectorPositionUpdated[i].x+"|"+vectorPositionUpdated[i].y+")");
 		} //end for
 		
 		//TransformationsMatrix aufbauen
 		transformationMatrix = scalingMatrix * rotationMatrix * translationMatrix;
-		
-		
+
+
+		//TODO: Rotationsmatrix außenvor lassen, da rotation auf der Stelle seperat erfolgt
+		//Oder evtl. TranslationIn0 * Rotation * TranslationInUrsprungsPosition?
+		//transformationMatrix = scalingMatrix * translationMatrix;
 	}
 }
